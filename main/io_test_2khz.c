178e168879950be5d3aadab708715e2269153d83
weizzh <weizzhster@gmail.com>
2020-05-25 17:55:42 +0800

    fix some warnings

--- main/io_test_2khz.c
+++ main/io_test_2khz.c
@@ -9,11 +9,11 @@
 #define IO_TEST 5 //
 #define TIMER_DIVIDER 16 
 #define TIMER_SCALE (TIMER_BASE_CLK / TIMER_DIVIDER) //TIMER_BASE_CLK is 80,000,000
-#define FRYQUENCY 256
+#define FRYQUENCY 2048
 #define TIMER_INTERVAL 1.0/FRYQUENCY 
 #define WITHOUT_RELOAD 0
 #define WITH_RELOAD 1
-static uint32_t intr_times = 0 ;//interr times
+static bool intr_times = 0 ;//interr times
 static void test_gpio_init(int io_test)
 {
 	gpio_pad_select_gpio(io_test);
@@ -24,11 +24,12 @@ void IRAM_ATTR timer_group0_isr(void *para)
 {
 	timer_spinlock_take(TIMER_GROUP_0);
 	int timer_idx = (int) para;
+	intr_times = !intr_times;
 	timer_group_clr_intr_status_in_isr(TIMER_GROUP_0, timer_idx);
-	gpio_set_level(IO_TEST, intr_times % 2); // to-do
+	gpio_set_level(IO_TEST, intr_times); // to-do
 	timer_group_enable_alarm_in_isr(TIMER_GROUP_0, timer_idx);
 	timer_spinlock_give(TIMER_GROUP_0);
-	intr_times ++;
+
 }
 
 static void test_timer_init(int timer_idx, bool auto_reload, double timer_interval_sec)
@@ -54,15 +55,11 @@ static void test_timer_init(int timer_idx, bool auto_reload, double timer_interv
 void app_main(void)
 {
 	test_gpio_init(IO_TEST);
-	printf('gpio inited\n');
+	printf("gpio inited\n");
 	test_timer_init(TIMER_0, WITH_RELOAD, TIMER_INTERVAL);
 	printf("timer inited\n");
 	while(1)
 	{
-		if( 100 == intr_times)
-		{
-			intr_times = 0;
-			printf("intr_times = 0\n");
-		}
+
 	}
 }
